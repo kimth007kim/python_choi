5147ad327aa47605b785bfd78d5845a0fe20e035
kimth007kim <kimth007kim@naver.com>
2020-10-04 19:46:46 +0900

    최교수님 파이썬 4주차 과제 제출

--- 과제/4주차/week4_5.py
+++ 과제/4주차/week4_5.py
@@ -1,15 +1,15 @@
 import random
 
-lotto = random.randrange(100)
+num = random.randint(1,99)
 your = int(input("복권번호를 입력하시오(0~99): "))
-print("당첨번호는", lotto, "입니다.")
+print("당첨번호는", num, "입니다.")
 
-a1 = lotto//10
-a2 = lotto%10
+a1 = num//10
+a2 = num%10
 b1 = your//10
 b2 = your%10
 
-if (lotto == your):
+if (num == your):
    print("상금은 100만원입니다.")
 elif (a1 == b1 or a1 == b2 or a2 == b1 or a2 == b2):
    print("상금은 50만원입니다.")
