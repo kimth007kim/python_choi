88cc9bb39c0f8e2ee966eb0be610439000626664
kimth007kim <kimth007kim@naver.com>
2020-10-08 18:40:27 +0900

    두근두근 파이썬 7장_7번까지

---
+++ Chap7/7_6.py
@@ -0,0 +1,9 @@
+def calculate_area (radius):
+    area = 3.14 * radius **2
+    return area
+
+c_area = calculate_area(5.0)
+print(c_area)
+
+area_sum = calculate_area(5.0)+ calculate_area(10.0)
+print(area_sum)
---
+++ Chap7/7_7.py
@@ -0,0 +1,6 @@
+def get_sum(start,end):
+    sum=0
+    for i in range(start,end+1):
+        sum +=i
+    return sum
+print(get_sum(5,10))
