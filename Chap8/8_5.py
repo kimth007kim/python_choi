f1a2f7a901183b2fa1d60ce9d0b8f5f650286e82
kimth007kim <kimth007kim@naver.com>
2020-10-26 17:58:33 +0900

    그림 넣는 에러를 고쳤다 고칠떄에는 image1="/Users/Kyungdong/Documents/python_choi/Chap8/rabbit.gif" 이런식으로 작성해야한다.

--- Chap8/8_5.py
+++ Chap8/8_5.py
@@ -1,23 +1,35 @@
-from turtle import Screen, Turtle
-from random import choice
-# os.chdir()
+# from turtle import Screen, Turtle
+# from random import choice
 
+import turtle
+import random
 
-screen=Screen()
-image1="/Users/Kyungdong/Documents/pythob_choi/Chap8/rabbit.gif"
-image2="/Users/Kyungdong/Documents/pythob_choi/Chap8/turtle.gif"
+screen= turtle.Screen()
+image1="/Users/Kyungdong/Documents/python_choi/Chap8/rabbit.gif"
+image2="/Users/Kyungdong/Documents/python_choi/Chap8/turtle.gif"
 
 screen.addshape(image1)
 screen.addshape(image2)
 
-turtle =Turtle()
 t1=turtle.Turtle()
 t1.shape(image1)
+t1.pensize(5)
+t1.penup()
+t1.goto(-300,0)
 
 t2=turtle.Turtle()
 t2.shape(image2)
+t2.pensize(5)
+t2.penup()
+t2.goto(-300,-200)
 
-# import os
-# print(os.getcwd())
-# os.chdir("C:\\python_choi\Chap8")
-# print(os.getcwd())
+t1.pendown()
+t2.pendown()
+t1.speed(1)
+t2.speed(1)
+
+for i in range(100):
+    d1= random.randint(1,60)
+    t1.forward(d1)
+    d2= random.randint(1,60)
+    t2.forward(d2)
