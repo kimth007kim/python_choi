6882401d87ba45f8000c73ce39b879c54454e210
kimth007kim <kimth007kim@naver.com>
2020-10-28 17:28:12 +0900

    9장들어감

---
+++ Chap8/8_12.py
@@ -0,0 +1,30 @@
+import turtle
+import math
+
+player=turtle.Turtle()
+player.shape("turtle")
+screen = player.getscreen()
+
+def turnleft():
+    player.left(5)
+
+def turnright():
+    player.right(5)
+
+def fire():
+    x=0
+    y=0
+    velocity=50
+    angle= player.heading()
+    vx= velocity *math.cos(angle * 3.14/ 180.0)
+    vy= velocity *math.sin(angle * 3.14/ 180.0)
+    while player.ycor()>=0:
+        vx = vx
+        vy =vy -10
+        x = x+vx
+        y = y+vy
+        player.goto(x,y)
+screen.onkeypress(turnleft,"Left")
+screen.onkeypress(turnright,"Right")
+screen.onkeypress(fire,"space")
+screen.listen() 
