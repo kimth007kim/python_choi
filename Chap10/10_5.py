5bc38a43cfa1b44a31edfc04bf977257cb852813
kimth007kim <kimth007kim@naver.com>
2020-11-05 18:36:05 +0900

    10_9까지 함

--- Chap10/10_5.py
+++ Chap10/10_5.py
@@ -1,4 +1,6 @@
 from tkinter import *
 
 window = Tk()
+button= Button(window,text="클릭하세요!")
+button.pack()
 window.mainloop()
---
+++ Chap10/10_9.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+def process():
+    print("안녕하세요?")
+
+window= Tk()
+button = Button(window,text="클릭하세요!",command=process)
+button.pack()
+window.mainloop()
