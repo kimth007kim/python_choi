037e0dc2168288510ec80fbf7165c438f34612cc
kimth007kim <kimth007kim@naver.com>
2020-12-07 22:20:21 +0900

    하교수님 과제 완료

---
+++ 파이썬forbeginner/Chap07List/pg179.py
@@ -0,0 +1,23 @@
+# a,b,c,d=0,0,0,0
+# hap=0
+
+# a= int(input("1번째 숫자"))
+# b= int(input("1번째 숫자"))
+# c= int(input("1번째 숫자"))
+# d= int(input("1번째 숫자"))
+
+# hap=a+b+c+d
+
+# print("합계==> %d" %hap)
+
+aa=[0,0,0,0]
+hap= 0
+
+aa[0]=int(input("1번째숫자"))
+aa[1]=int(input("2번째숫자"))
+aa[2]=int(input("3번째숫자"))
+aa[3]=int(input("4번째숫자"))
+
+hap= aa[0]+aa[1]+aa[2]+aa[3]
+
+print("합계==> %d" %hap)
---
+++ 파이썬forbeginner/Chap07List/pg181.py
@@ -0,0 +1,7 @@
+aa = []
+
+for i in range(0,100):
+    aa.append(0)
+
+len(aa)
+# print(aa)
